643a07e4bc8d8d3a71fe0fb0d61d1e7c84b1cb07
BunnyPrince <jananee1999@yahoo.com>
2025-12-16 23:54:10 -0500

    move addToCart in cart.js

--- data/cart.js
+++ data/cart.js
@@ -1,1 +1,20 @@
 export const cart = [];
+
+export function addToCart(productId){
+  let matchingItem;
+      
+  cart.forEach((CartItem) => {
+    if(CartItem.productId === productId){
+      matchingItem = CartItem;
+    }
+  })
+
+  if(matchingItem){
+    matchingItem.quantity++;
+  }else{
+    cart.push({
+      productId: productId,
+      quantity: 1
+    });
+  }
+}
