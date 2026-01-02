583b6437db8e135725a140df7b4aa6161f20afc4
BunnyPrince <jananee1999@yahoo.com>
2026-01-02 14:02:06 -0500

    creat class version of cart

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,7 +1,8 @@
 import { cart } from "../data/cart.js";
 import { renderOrderSummary } from "./checkout/orderSummary.js";
 import { renderPaymentSummary } from "./checkout/paymentSummary.js";
-import '../data/cart-oop.js';
+// import '../data/cart-oop.js';
+import '../data/cart-class.js';
 
 let itemQuantity = 0;
 
